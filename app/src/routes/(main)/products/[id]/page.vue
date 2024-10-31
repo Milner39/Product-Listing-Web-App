0c1b1f31247f13864f107a390ae0a9f4b956d72b
Finn Milner <finn.milner@outlook.com>
2024-10-31 13:43:12 +0000

    feat: basic product page

--- app/src/routes/(main)/products/[id]/page.vue
+++ app/src/routes/(main)/products/[id]/page.vue
@@ -19,5 +19,90 @@ const { data: productData } = await getProduct()
 </script>
 
 <template>
-    {{ productData }}
+    <div class="product__wrapper">
+        <div class="product">
+            <div class="product__image__wrapper">
+                <img class="product__image" :src="productData?.thumbnail"/>
+            </div>
+            <div class="product__title">
+                <h4>{{ productData?.title }}</h4>
+                <p v-if="productData?.brand">{{ productData.brand }}</p>
+            </div>
+            <p class="product__price"><strong>Retails at:</strong> £{{ productData?.price }}</p>
+            <p class="product__description">{{ productData?.description }}</p>
+            <div class="product__reviews__wrapper">
+                <h5>Reviews:</h5>
+                <ul class="product__reviews style-reset">
+                    <li v-for="review in productData?.reviews" class="reviews__review__wrapper">
+                        <div class="reviews__review">
+                            <p class="review__reviewer-name"><strong>{{ review.reviewerName }}</strong></p>
+                            <p class="review__text">{{ review.comment }}</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+
+.product__wrapper {
+    padding: 1rem;
+
+    > .product {
+        display: flex;
+        gap: 1rem;
+        flex-direction: column;
+
+        width: min(64ch, 100%);
+
+        margin-inline: auto;
+
+        > .product__image__wrapper {
+            > .product__image {
+                width: 100%;
+            }
+        }
+
+        > .product__title {
+            display: grid;
+            grid-template-columns: 1fr auto;
+            gap: 1ch;
+            align-items: center;
+
+            p {
+                margin-top: 0.2rem; // Weird alignment 
+            }
+        }
+
+        > .product__reviews__wrapper {
+            display: flex;
+            gap: 0.5rem;
+            flex-direction: column;
+
+            > .product__reviews {
+                display: flex;
+                gap: 0.75rem;
+                flex-direction: column;
+            }
+        }
+
+        > * {
+            position: relative;
+            z-index: -1;
+        }
+
+        > * + *::before {
+            content: "";
+            position: absolute;
+            top: -0.5rem;
+            width: 100%;
+            height: 1px;
+            background-color: gray;
+        }
+    }
+        
+}
+
+</style>
