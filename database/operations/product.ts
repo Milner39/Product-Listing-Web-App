f855e163c7be64a74a37554e409cabfc650fe4a4
Finn Milner <finn.milner@outlook.com>
2024-10-31 17:10:52 +0000

    chore: add type checking

--- database/operations/product.ts
+++ database/operations/product.ts
@@ -6,11 +6,19 @@ import { dbClient, tables } from "../dbUtils"
 
 
 
+// #region Types
+
+type Insert = typeof tables.product.$inferInsert
+type Select = typeof tables.product.$inferSelect
+
+// #endregion Types
+
+
 // #region Operations
 
-const create = async (fieldValues) => {
+const create = async (fieldValues: Insert) => {
     try {
-        const entry = (await dbClient.insert(tables.product).values(fieldValues).returning())[0]
+        const entry: Select = (await dbClient.insert(tables.product).values(fieldValues).returning())[0]
     } catch (error) {
         console.error(`At: ${import.meta.url}:`)
         console.error(error instanceof Error ? error.stack : error)
