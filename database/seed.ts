619c212efb69bc4f1872656983838197227c5e5e
Finn Milner <finn.milner@outlook.com>
2024-11-01 12:09:37 +0000

    docs

--- database/seed.ts
+++ database/seed.ts
@@ -28,8 +28,21 @@ try {
         total: number
     }
 
+    // Get response data
     const responseData: ResponseData = await response.json()
+
+    // For each product returned, create a product in the database 
     for (const product of responseData.products) {
+
+        /*
+            At the moment, a flat-file database is being used.
+            This is only because of the time constraint on this project
+            but it means any Arrays must be stringified to be stored in
+            the database. A relational database would make this obsolete,
+            I just don't have the time.
+
+            This loop will stringify the necessary columns.
+        */
         // @ts-ignore
         product.tags = JSON.stringify(product.tags)
         // @ts-ignore
@@ -41,6 +54,7 @@ try {
         // @ts-ignore
         product.meta = JSON.stringify(product.meta)
 
+        
         productTable.create(product)
     }
 
